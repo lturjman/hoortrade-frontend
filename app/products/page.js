5db19d2eadb5c22f7bf13e982987aaba5549ed7b
Laura Turjman <contact@lauraturjman.fr>
2025-09-30 18:00:46 +0200

    clean useless code

--- app/products/page.js
+++ app/products/page.js
@@ -5,12 +5,10 @@ import LogoutButton from "@/components/auth/LogoutButton";
 export default function Login() {
   return (
     <main className="relative min-h-screen flex flex-col items-center justify-center p-4 space-y-6 bg-zinc-50 dark:bg-zinc-800">
-      {/* Bouton logout fixé en haut à droite */}
       <div className="absolute top-4 right-4">
         <LogoutButton />
       </div>
 
-      {/* On décale le contenu pour ne pas que le bouton le recouvre */}
       <div className="pt-16 text-center">
         <Link href="/">
           <h1 className="text-4xl font-bold">
