5da8dbeb1919bba62290c28864b6ba06751a0b62
Laura Turjman <contact@lauraturjman.fr>
2025-09-30 18:00:10 +0200

    change logoutButton Place

--- components/auth/LogoutButton.js
+++ components/auth/LogoutButton.js
@@ -5,6 +5,7 @@ import Button from "@/components/Button";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { clearUsers } from "@/lib/store/slices/users";
+import { UserIcon } from "@heroicons/react/24/solid";
 
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -40,8 +41,9 @@ export default function LogoutForm() {
     <div>
       <Button
         onClick={handleLogout}
-        className="px-4 py-2 bg-red-400 text-white rounded-full hover:bg-red-500 active:bg-red-600"
+        className="px-4 py-2 bg-red-400 text-white rounded-full hover:bg-red-500 active:bg-red-600 gap-2"
       >
+        <UserIcon className="h-4" />
         Déconnexion
       </Button>
     </div>
